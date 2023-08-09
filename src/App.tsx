19098e5449121eede85edc4a6a1ace4e32005db6
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2023-08-09 16:54:09 +0300

    Update custom theme usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, ThemeProvider } from '@reportportal/ui-kit';
+import Button from '@reportportal/ui-kit/dist/button';
+import { ThemeProvider } from '@reportportal/ui-kit/dist/themeProvider';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Button>Button</Button>
-        <Button variant="danger">Button</Button>
         <ThemeProvider theme="light">
           <Button>Light theme button</Button>
         </ThemeProvider>
-        <ThemeProvider customThemes={{ 'my-theme': 'my-custom-theme' }} theme="my-theme">
-          <Button>Custom theme button</Button>
-          <ThemeProvider theme="dark">
-            <Button>Dark theme button override</Button>
-          </ThemeProvider>
+        <ThemeProvider theme="dark">
+          <Button>Dark theme button</Button>
         </ThemeProvider>
+        <div className="Custom-theme">
+          Custom theme
+          <br />
+          <ThemeProvider customThemes={{ 'my-theme': 'my-custom-theme' }} theme="my-theme">
+            <Button>Custom theme button</Button>
+            <ThemeProvider theme="dark">
+              <Button>Dark theme button override</Button>
+            </ThemeProvider>
+          </ThemeProvider>
+        </div>
       </header>
     </div>
   );
