58195638c6595e4279785fc65d25c7360875f717
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2023-08-16 13:26:10 +0300

    Add more components to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from '@reportportal/ui-kit/dist/button';
-import { ThemeProvider } from '@reportportal/ui-kit/dist/themeProvider';
+// @ts-ignore
+import Button from '@reportportal/ui-kit/dist/button.js';
+import { ThemeProvider, Checkbox, SystemMessage, FieldText } from '@reportportal/ui-kit';
 
 function App() {
+  const [name, setName] = React.useState('');
+  const [checked, setChecked] = React.useState(true);
+
+  const onNameChange = (event: any) => {
+    setName(event.target.value);
+  }
+
+  const onChange = (event: any) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <FieldText placeholder="Type smth here" value={name} onChange={onNameChange} clearable onClear={() => setName('')} defaultWidth />
+        <SystemMessage header="Header" caption="Caption" widthByContent>Message</SystemMessage>
         <Button>Button</Button>
         <ThemeProvider theme="light">
           <Button>Light theme button</Button>
         </ThemeProvider>
         <ThemeProvider theme="dark">
           <Button>Dark theme button</Button>
+          <Checkbox value={checked} onChange={onChange}>Checkbox dark</Checkbox>
         </ThemeProvider>
         <div className="Custom-theme">
           Custom theme
