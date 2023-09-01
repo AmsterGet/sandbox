11306c1403bb8068b365eda31f2d059c8f8ff657
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2023-09-01 17:56:37 +0300

    Add more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
-import { ThemeProvider, Checkbox, SystemMessage, FieldText, Modal, Button } from '@reportportal/ui-kit';
+import { DropdownValue } from "@reportportal/ui-kit/dist/components/dropdown/types";
+import {
+  ThemeProvider,
+  Checkbox,
+  SystemMessage,
+  FieldText,
+  Modal,
+  Button,
+  Dropdown,
+  FieldNumber,
+  Toggle,
+  Icon,
+  BubblesLoader,
+} from '@reportportal/ui-kit';
 
 const ModalContent = () => {
   const [name, setName] = React.useState('');
   const [checked, setChecked] = React.useState(false);
+  const [value, setValue] = React.useState<string | number>('0');
+  const handleChange = (val: string | number) => {
+    setValue(val);
+  };
 
   const onChangeCheckmark = (event: any) => {
     setChecked(event.target.checked);
@@ -20,6 +37,13 @@ const ModalContent = () => {
 
   return (
       <div>
+        <div>
+          <Checkbox value={checked} onChange={onChangeCheckmark}>Check me</Checkbox>
+        </div>
+        <div>
+          <Toggle value={checked} onChange={onChangeCheckmark}>Check me</Toggle>
+        </div>
+        <FieldNumber value={value} onChange={handleChange} />
         {!checked && (
             <FieldText
                 label="What is your name?"
@@ -31,9 +55,38 @@ const ModalContent = () => {
                 onClear={onClearName}
             />
         )}
-        <Checkbox value={checked} onChange={onChangeCheckmark}>Check me</Checkbox>
+        <div>
+          <Icon icon="delete" />
+        </div>
       </div>
   );
+}
+
+const DropdownField = () => {
+  const [activeOption, setActiveOption] = React.useState<DropdownValue>(1);
+
+  const dropdownOptions = [
+    { value: 1, label: 'One' },
+    { value: 2, label: 'Two' },
+    { value: 4, label: '4' },
+    { value: 5, label: '5' },
+    { value: 6, label: '6' },
+    { value: 7, label: '7' },
+    { value: 8, label: '8' },
+    { value: 9, label: '9' },
+  ];
+
+  const onChangeOption = (value: DropdownValue) => {
+    setActiveOption(value);
+  }
+
+  return (
+      <Dropdown
+          value={activeOption}
+          options={dropdownOptions}
+          onChange={onChangeOption}
+      />
+  )
 }
 
 function App() {
@@ -54,8 +107,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SystemMessage header="Header" caption="Caption" widthByContent>Message</SystemMessage>
-        <Button type="submit" onClick={showModalAction}>Button</Button>
+        <SystemMessage header="Header" caption="Caption" widthByContent>
+          Message
+          <BubblesLoader />
+        </SystemMessage>
+        <Button type="submit" onClick={showModalAction}>Show modal</Button>
         <ThemeProvider theme="light">
           <Button>Light theme button</Button>
         </ThemeProvider>
@@ -86,10 +142,7 @@ function App() {
               overlay="light-cyan" onClose={closeModalAction}
             >
               <ModalContent />
-              <ModalContent />
-              <ModalContent />
-              <ModalContent />
-              <ModalContent />
+              <DropdownField />
             </Modal>
         )}
       </header>
